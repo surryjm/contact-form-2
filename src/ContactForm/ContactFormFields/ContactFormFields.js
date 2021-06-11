47c1437d618c73bb671c8835e64f3a377c470061
Surry <50641921+surryjm@users.noreply.github.com>
2021-06-10 22:47:37 -0700

    edited axios response

--- src/ContactForm/ContactFormFields/ContactFormFields.js
+++ src/ContactForm/ContactFormFields/ContactFormFields.js
@@ -49,7 +49,7 @@ export default class ContactFormFields extends Component {
       }
     })
     .then((response) => {
-      console.log(response);
+      console.log("Response: " + response.data.status);
     })
     .catch((error) => {
       console.log(error.response.data);
