04fbd8b02189e2f1db385283cfab03eaed5a4ed2
Surry <50641921+surryjm@users.noreply.github.com>
2021-06-10 21:35:44 -0700

    logging axios errors

--- src/ContactForm/ContactFormFields/ContactFormFields.js
+++ src/ContactForm/ContactFormFields/ContactFormFields.js
@@ -53,6 +53,10 @@ export default class ContactFormFields extends Component {
     })
     .catch((error) => {
       console.log(error);
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      console.log(error.toJSON());
     })
 
 
